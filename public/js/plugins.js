7c7adbd57447c44d6c49150cc676ea33ba74c163
JKasyan <j.kasyan86@gmail.com>
2016-09-21 16:56:21 +0300

    Sort table plugin

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,43 +1,50 @@
 (function($){
 
+    'use strict';
+
     var isAscending = true;
 
-    $.fn.sortTable = function() {
-        var $this = $(this);
-        console.log('$this = ', $this)
-        var columns = $(this).find('thead th');
-        console.log('columns = ',columns.length);
-        for(var i = 0;i<columns.length;i++) {
-            var th = columns[i];
-            (function (i) {
-                $(th).on('click', function () {
-                    console.log('click...', i);
-                    _sort(i, $this);
-                });
-            })(i);
+    var methods = {
+        init:function(){
+            var $this = this;
+            return this.find('thead th').each(function(x){
+                var th = $(this);
+                (function (i) {
+                    $(th).bind('click', function () {
+                        sortListener(i, $this);
+                    });
+                })(x);
+            });
+        },
+        destroy: function () {
+            return this.find('thead th').each(function(){
+                $(this).unbind();
+            })
         }
     }
 
-    function _sort(i, $this) {
+    $.fn.sortTable = function(method) {
+        if(methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method == 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('This method ' + method + ' is absent.');
+        }
+    }
+
+    function sortListener(i, $this) {
         var rows = $($this).find('tbody tr').clone();
         rows.sort(function(a, b) {
             var tdFirst = $(a).find('td')[i];
             var valueFirst = $(tdFirst).html().trim();
             var tdSecond = $(b).find('td')[i];
             var valueSecond = $(tdSecond).html().trim();
-            if(isAscending) {
-                return valueFirst - valueSecond;
-            } else {
-                return valueSecond - valueFirst;
-            }
+            return (isAscending) ? (valueFirst - valueSecond) : (valueSecond - valueFirst);
         });
         isAscending = !isAscending;
         $($this).find('tbody tr').remove();
         $($this).find('tbody').append(rows);
-        console.log('Min = ', rows[0]);
-        console.log('Max = ', rows[rows.length - 1]);
-        //
-
     }
 
 })(jQuery);
